20b17c356366b9f72949c7cc32f4c782b0a816d0
上村龍太 <uemuraryuuta@uemuraryuutanoMacBook-Air.local>
2024-01-26 11:34:54 +0900

    カレンダー課題、レビュー後修正

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -11,13 +11,12 @@ end.parse!
 options[:month] = Date.today.month if options[:month].nil?
 
 unless (1..12).include?(options[:month].to_i)
-  puts "#{options[:month]} is neither a month number (1..12) nor a name"
-  exit
+  raise "#{options[:month]} is neither a month number (1..12) nor a name"
 end
 
 date = Date.new(Date.today.year, options[:month].to_i, 1)
 
-puts "      #{date.strftime('%B %Y')}"
+puts date.strftime('%B %Y').center(20)
 puts "月 火 水 木 金 土 日"
 
 first_day = date.wday
