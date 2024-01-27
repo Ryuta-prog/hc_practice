21ebfa77e5268fb2f7be54275f41963ea2944a50
上村龍太 <uemuraryuuta@uemuraryuutanoMacBook-Air.local>
2024-01-27 15:18:45 +0900

    レビュー後修正提出用240127vending-machine.rb

--- ruby/vending_machine/juice.rb
+++
@@ -1,8 +0,0 @@
-class Juice
-    attr_reader :name, :price
-
-    def initialize(name, price)
-      @name = name
-      @price = price
-    end
-end
--- ruby/vending_machine/vending-machine.rb
+++ ruby/vending_machine/vending-machine.rb
@@ -17,8 +17,12 @@ class VendingMachine
     raise "残高が不足しています" if suica.balance < selected_juice.price
 
     suica.pay(selected_juice.price)
-    selected_juice.sell
+    selected_juice.stock -= 1
     @sales += selected_juice.price
+  end
+
+  def remove_juice(juice_name)
+    @juices.reject! { |juice| juice.name == juice_name }
   end
 
   def stock_info
