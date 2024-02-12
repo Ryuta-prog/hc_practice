650742b9bc245a4b8a0249574324b65ae909e96a
上村龍太 <uemuraryuuta@uemuraryuutanoMacBook-Air.local>
2024-02-12 22:33:34 +0900

    vending-machine.rb

--- ruby/vending_machine/vending-machine.rb
+++ ruby/vending_machine/vending-machine.rb
@@ -5,16 +5,14 @@ class VendingMachine
     @juices = [
       5.times { Juice.new('ペプシ', 150) },
       5.times { Juice.new('モンスター', 230) },
-https://prod.liveshare.vsengsaas.visualstudio.com/join?17A74485269E83868A1B7370E28F66FC1449https://prod.liveshare.vsengsaas.visualstudio.com/join?17A74485269E83868A1B7370E28F66FC1449class VendingMachine
-5.times { Juice.new('いろはす', 120) }
+      5.times { Juice.new('いろはす', 120) }
     ]
     @sales = 0
   end
 
   def buy(juice_name, suica)
-    selected_juice = @juices.find { juice.name == juice_name }
-
-    raise "#{juice_name}の在庫がありません" if selected_juice.stock < 1
+    selected_juice = @juices.select { |juice| juice.name == juice_name }
+    raise "#{juice_name}の在庫がありません" if selected_juice < 1
     raise "残高が不足しています" if suica.balance < selected_juice.price
 
     suica.pay(selected_juice.price)
@@ -23,14 +21,14 @@ https://prod.liveshare.vsengsaas.visualstudio.com/join?17A74485269E83868A1B7370E
   end
 
   def remove_juice(juice_name)
-    @juices.reject! { |juice| juice.name == juice_name }
+    @juices.select { |juice| juice.name == juice_name }
   end
 
-  def stock_info
-    @juices.map { |juice| "#{juice.name}: #{juice.stock}本" }.join(", ")
+  def sales_info
+    @juices.select { |juice| "#{juice.name}: #{juice.select}本" }.join(", ")
   end
 
   def purchasable_list(suica)
-    @juices.select { |juice| juice.stock > 0 && suica.balance >= juice.price }.map(&:name)
+    @juices.select { |juice| juices > 0 && suica.balance >= juice.price }.map(&:name)
   end
 end
