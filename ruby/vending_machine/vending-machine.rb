ccf54d70bb30c7422111d0f00cc765df2bef889e
Ryuta-prog <longtaishangcun@gmail.com>
2024-05-23 21:33:29 +0900

    vending-machine.rb

---
+++ ruby/vending_machine/vending-machine.rb
@@ -0,0 +1,26 @@
+class VendingMachine
+    attr_reader :sales
+  
+    def initialize
+      @juices = 5.times { [Juice.new('ペプシ', 150), Juice.new('モンスター', 230), Juice.new('いろはす', 120)]}
+      @sales = 0
+    end
+  
+    def buy(juice_name, suica)
+      selected_juice = @juices.count { |juice| juice.name == juice_name }
+      raise "#{juice_name}の在庫がありません" if selected_juice < 1
+      raise "残高が不足しています" if suica.balance < selected_juice.price
+  
+      suica.pay(selected_juice.price)
+      selected_juice -= 1
+      @sales += selected_juice.price
+    end
+  
+    def sales_info
+      @sales
+    end
+  
+    def purchasable_list(suica)
+      @juices.select { |juice| juices > 0 && suica.balance >= juice.price }.map(&:name)
+    end
+  end
